605ed9b51391d6a7726143df5b7fa58159ace271
eliza galea <111540402+egalea504@users.noreply.github.com>
2023-03-25 23:01:38 -0400

    add load new tweets featureÈ

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,46 +4,45 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+// const data = [
+//   {
+//     "user": {
+//       "name": "Newton",
+//       "avatars": "https://i.imgur.com/73hZDYK.png"
+//       ,
+//       "handle": "@SirIsaac"
+//     },
+//     "content": {
+//       "text": "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//     "created_at": 1461116232227
+//   },
+//   {
+//     "user": {
+//       "name": "Descartes",
+//       "avatars": "https://i.imgur.com/nlhLi3I.png",
+//       "handle": "@rd" },
+//     "content": {
+//       "text": "Je pense , donc je suis"
+//     },
+//     "created_at": 1461113959088
+//   }
+// ]
+
+$(document).ready(function() {
 
 const renderTweets = function(tweets) {
   // loops through tweets
   for (let tweet of tweets) {
-    $(document).ready(function() {
-  
       const $tweet = createTweetElement(tweet);
       $('#tweet-container').append($tweet);
-    });
   }
   // calls createTweetElement for each tweet
   // takes return value and appends it to the tweets container
 }
 
 const createTweetElement = function(tweet) { 
-  let now = new Date();
+  let now = timeago.format(tweet.created_at);
   //timestamp wasn't giving a valid date
   // let createdAt = new Date(tweet.created_at);
   // let timePassed = now - createdAt;
@@ -68,4 +67,20 @@ const createTweetElement = function(tweet) {
 return finalHTML;
 };
 
-renderTweets(data);
+$("#form-tweet").submit(function(event) {
+    event.preventDefault();
+    let data = $( this ).serialize()
+    $.ajax({url: "/tweets/", method: 'POST', data: data})
+})
+
+// load tweets which have been posted
+const loadTweets = $(function() {
+  $.ajax({url: "/tweets/", method: 'GET', datatype: 'json'})
+  .then(function(data) {
+    console.log('Success: ', data);
+    //empty so the page can load all new tweets
+    $('#tweet-container').empty();
+    renderTweets(data);
+  })
+})
+});
