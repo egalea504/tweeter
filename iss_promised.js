326c8db9c080addef986d2d47160f223f9c53030
eliza galea <111540402+egalea504@users.noreply.github.com>
2023-04-02 09:18:26 -0400

    add image final product

--- iss_promised.js
+++
@@ -1,6 +0,0 @@
-const request = require('request-promise-native');
-
-const fetchMyIP = new Promise((resolve, reject) => {
-
-})
-  request('https://api.ipify.org?format=json', (error, response, body) => {
